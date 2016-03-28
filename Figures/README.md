d1ed63b611183f72ea0b13a6a3c8a6082b45a1d3
Daisy Leung <leungtszkuk@gmail.com>
2016-03-27 23:48:28 -0400

    take into account DR's comments on cosmetics

--- Figures/README.md
+++ Figures/README.md
@@ -17,3 +17,30 @@ SED_ParamProb__thinNopower.eps from ../src/SED/
 
 ### hst_rgb_cube.eps
 - not corrected for astrometry
+
+### F555W.eps
+- just HST F555W image
+
+### CO_highOmom_CLIP5sigma.eps
+- 1st and 2nd moment maps of CO(2-1)
+- clipped at 5 sigma
+- see 26Dec15 for velocity ranges and contour steps
+
+### spatialSpec_offsetShifted.eps
+- spatial spectra of CO(2-1), where (0,0) is the foreground galaxy
+
+### spatialSpec.eps
+- spatial spectra of CO(2-1), where (0,0) is the obs. phase center
+
+### Cont2mm_5GHz_single.eps, Cont2mm_5GHz_double.eps
+- continuum overlays of PdBI 2mm and VLA C Band
+
+### co\_channel_maps.eps
+- CO(2-1) channel map with markers from HST (HSTmarkers.txt)
+
+### SpecCO21_twinx.eps
+- spatially integrated spectrum of CO(2-1)
+
+
+### co32_spec_bin2.eps 
+- CO(3-2) line profile
--- PdBI/src/pythonPLOT/COChan.py
+++ PdBI/src/pythonPLOT/COChan.py
@@ -1,11 +1,19 @@
 '''
-Last Modified: 31 Dec 15
+Last Modified: 27 Mar 16
 
 
 Author: Daisy Leung
 
 
 History:
+27 Mar 2016
+- change symbol for foreground galaxy
+- increment contour by 3sigma instead of 4 sigma
+- add channels before first and after last
+- don't put legend for markers (don't fit in there now with more panels)
+- change colorbar ticks to mJy B^-1
+- change xaxis tick frequency to avoid overlap labeling
+
 31 Dec 2015
 - update HST coordinates, see HSTmarkers.txt
 
@@ -31,7 +39,7 @@ Note:
 -
 
 '''
-
+import matplotlib
 import matplotlib.pyplot as plt
 import pywcsgrid2
 import pywcs
@@ -43,7 +51,14 @@ import mpl_toolkits.axes_grid1.axes_grid as axes_grid
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 from mpl_toolkits.axes_grid1.anchored_artists import AnchoredText
 
+font = {'family': 'Arial Narrow',
+        'weight': 'bold',
+        'size': 8.75}
+matplotlib.rc('font', **font)
+
+
 class Velo(object):
+
     def __init__(self, header):
         wcs = pywcs.WCS(header)
         self.wcs_vel = wcs.sub([3])        # velocity is 3rd axis
@@ -56,14 +71,14 @@ class Velo(object):
 def setup_axes(fig, header, nx, ny):
 
     gh = pywcsgrid2.GridHelper(wcs=header)
-    gh.locator_params('x', nbins=3)
+    gh.locator_params('x', nbins=2)
 
     g = axes_grid.ImageGrid(fig, 111,
                             nrows_ncols=(ny, nx),
                             ngrids=None,
                             direction='row',
                             # axes_pad=0.02,
-                            axes_pad = 0.0,    # between panels
+                            axes_pad=0.0,    # between panels
                             add_all=True,
                             share_all=True,
                             # if share_all = False, need the following
@@ -76,16 +91,17 @@ def setup_axes(fig, header, nx, ny):
 
     # only show xylabel at that panel, depending on nrows_ncols in axes_grid.ImageGrid()
     # if comment out the following block, will shorten to Greek Letters
-    ax = g[-1*nx]     # display in the bottom left corner only
+    ax = g[-1 * nx]     # display in the bottom left corner only
     ax.set_xlabel("Right Ascension (J2000)")
     ax.set_ylabel("Declination (J2000)")
-    # other attributes: http://matplotlib.org/mpl_toolkits/axes_grid/api/axis_artist_api.html
+    # other attributes:
+    # http://matplotlib.org/mpl_toolkits/axes_grid/api/axis_artist_api.html
 
     # make colorbar
     ax = g[-1]
     cax = inset_axes(ax,
-                     width="8%", # # width = 8% width = 10% of parent_bbox width
-                     height="100%", # height : 50%
+                     width="8%",  # width = 8% width = 10% of parent_bbox width
+                     height="100%",  # height : 50%
                      loc=3,    # 4
                      bbox_to_anchor=(1.01, 0, 1, 1),
                      bbox_transform=ax.transAxes,
@@ -95,9 +111,10 @@ def setup_axes(fig, header, nx, ny):
     return g, cax
 
 Plotpath = '/Users/admin/Research/RXJ1131/Figures/'
-fits_cube = pyfits.open("/Users/admin/Research/RXJ1131/PdBI/data/04Sep15/sup127_155_2ndcln_noCont.fits")
-dxy = 125                   # starting channel
-nx = 6
+fits_cube = pyfits.open(
+    "/Users/admin/Research/RXJ1131/PdBI/data/04Sep15/sup127_155_2ndcln_noCont.fits")
+dxy = 124                   # starting channel
+nx = 7
 ny = 5
 nxy = nx * ny               # total number of channels
 sigma = 1.451e-3            # sigma in map for contour plots
@@ -112,9 +129,10 @@ major_px = header['BMAJ'] * 3600 / cdelt1                 # arcsrc/pix
 minor_px = header['BMIN'] * 3600 / cdelt2
 BPA_deg = header['BPA']         # should be 13 deg
 
-wcs = pywcs.WCS(header).sub([1, 2])       # .sub([1,2]) needed for wcs.wcs_pix2sky(px, py, 1) later in the code, flattens cube axes
+# .sub([1,2]) needed for wcs.wcs_pix2sky(px, py, 1) later in the code, flattens cube axes
+wcs = pywcs.WCS(header).sub([1, 2])
 vel = Velo(header)
-fig = plt.figure(figsize=(12, 12), dpi=100)      # don't change dpi, disastrous
+fig = plt.figure(figsize=(12, 15), dpi=100)      # don't change dpi, disastrous
 plt.subplots_adjust(top=0.9)
 g, cax = setup_axes(fig, header, nx, ny)
 
@@ -125,50 +143,63 @@ cmap = plt.cm.jet
 import matplotlib.colors as mcolors
 norm = mcolors.Normalize()
 images = []
-start_channel = i*nxy+dxy
+start_channel = i * nxy + dxy
 
 for i, ax in enumerate(g):
     channel_number = start_channel + i
     channel = fits_cube[0].data[0][channel_number]
     im = ax.imshow(channel, origin="lower", norm=norm, cmap=cmap)
+    cont_list = [sigma * i for i in range(-6, 24, 3) if i != 0]
     ax.contour(channel,
-               [-4*sigma, 4*sigma, 8*sigma, 12*sigma, 16*sigma, 20*sigma],
+               cont_list,
                colors='black')
 
     # zoom-in on pixels
-    ax.set_xlim(113, 148)
-    ax.set_ylim(107, 142)
+    ax.set_xlim(110, 150)
+    ax.set_ylim(107, 147)
 
     if HST:   # mark lensing knots
         import astrolib.coords as coords
         # to convert sexidesimal coord --> deg in tuple
         p_list = [
-                  coords.Position("11:31:51.44295 -12:31:58.30659").j2000(), # FG G
-                  coords.Position("11:31:51.5769 -12:31:58.9708").j2000(), # A
-                  coords.Position("11:31:51.5678 -12:31:57.8143").j2000(), # B
-                  coords.Position("11:31:51.5366 -12:32:00.1192").j2000(),  # C
-                  coords.Position("11:31:51.3658 -12:31:58.1183").j2000()    # D
-                ]
+            coords.Position("11:31:51.5769 -12:31:58.9708").j2000(),  # A
+            coords.Position("11:31:51.5678 -12:31:57.8143").j2000(),  # B
+            coords.Position("11:31:51.5366 -12:32:00.1192").j2000(),  # C
+            coords.Position("11:31:51.3658 -12:31:58.1183").j2000()    # D
+        ]
         for p1 in p_list:
             ra, dec = p1
             ra_px, dec_px = wcs.wcs_sky2pix(ra, dec, 1)
             l1, = ax.plot([ra_px], [dec_px],
-                          "k+", mec="black", mew=1.5, ms=8,
+                          color='white',
+                          marker='+',
+                          mec="black", mew=1.25, ms=6,
                           zorder=3.1)    # lower zorder are drawn first
+
+        # fg Galaxy
+        fg_list = [coords.Position("11:31:51.44295 -12:31:58.30659").j2000()]
+        for p in fg_list:
+            ra, dec = p
+            ra_px, dec_px = wcs.wcs_sky2pix(ra, dec, 1)
+            l2, = ax.plot([ra_px], [dec_px],
+                          color='white',
+                          marker="*", mec="black", mew=1.5, ms=6,
+                          zorder=3.1)    # lower zorder are drawn first
+
     images.append(im)
 
 
-if HST:
-   # put up legend on last panel --> HST markers
-    g[0].legend([l1],
-                ["HST QSO lensing knots"],
-                loc=3,
-                numpoints=1,
-                handlelength=1,
-                frameon=True,
-                framealpha=0.8,
-                prop=dict(size=8),
-                fancybox=True)
+# if HST:
+#    # put up legend on last panel --> HST markers
+#     g[0].legend([l1, l2],
+#                 ["QSO lensing knots", "Lensing Galaxy"],
+#                 loc=3,
+#                 numpoints=1,
+#                 handlelength=1,
+#                 frameon=True,
+#                 framealpha=0.8,
+#                 prop=dict(size=8),
+#                 fancybox=True)
 
 
 # label with velocities
@@ -188,7 +219,7 @@ for i, ax in enumerate(g):
     t.patch.set_alpha(1.0)
     if use_path_effect:
         t.txt._text.set_path_effects([withStroke(foreground="w",
-                                                 linewidth=3)])
+                                                 linewidth=2.5)])
     else:
         # show velocity text in some color
         t.txt._text.set_color('k')
@@ -201,8 +232,11 @@ g[-1].add_beam_size(minor_px, major_px, BPA_deg,      # y, x, angle
 
 # make colorbar
 cb = plt.colorbar(im, cax=cax)
-cb.set_label("Flux Density [Jy B"+r"$^{-1}$]")
-cb.set_ticks([0, 0.01, 0.02, 0.03])
+cb.set_label("Flux Density [mJy B" + r"$^{-1}$]")
+cb.set_ticks([-0.01, 0, 0.01, 0.02, 0.03])   # place tick locations
+
+# customize to mJy B^-1 instead of Jy B^-1, uncomment to use same as cb.set_ticks
+cb.set_ticklabels(['-10', '0', '10', '20', '30'])
 
 # adjust norm
 # norm.vmin = -0.007      # min in map
@@ -211,13 +245,14 @@ norm.vmax = 0.03          # max in map
 for im in images:
     im.changed()
 
-fig.suptitle("CO(2-1) Channel Maps", fontsize=22, x=0.5, y=0.92)
+# fig.suptitle("CO(2-1) Channel Maps", fontsize=22, x=0.5, y=0.92)
+fig1 = plt.gcf()
 plt.show()
 fits_cube.close()
 
 User_input = raw_input('Save figure? (Y/N): ')
 if User_input == 'Y':
     filename = "co_channel_maps.eps"
-    plt.savefig(Plotpath + filename, dpi=70, bbox_inches="tight", pad_inches=0.1)
-    print "-- Saved figure as : %s --" %(Plotpath + filename)
-
+    fig1.savefig(Plotpath + filename, dpi=100,
+                bbox_inches="tight", pad_inches=0.1)
+    print "-- Saved figure as : %s --" % (Plotpath + filename)
