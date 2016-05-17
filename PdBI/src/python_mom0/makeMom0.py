79bd5b07329e90e71192c6fd33de4cc7b6854d81
Daisy Leung <leungtszkuk@gmail.com>
2016-05-16 20:12:29 -0400

    mom0 multiplied by chan width

--- PdBI/src/python_mom0/makeMom0.py
+++ PdBI/src/python_mom0/makeMom0.py
@@ -5,18 +5,28 @@ look through histogram again for each channel
 
 add the channels manually to create moment-0 map, and look at historgram after
 
-since the noise properties look ok, make a FITS file using this with header from the GILDAS moment0 map (which can potentially cause trouble in the future, but run with it for now) for APLpy plot.
+since the noise properties look ok, make a FITS file using this with header from the GILDAS moment0 map for APLpy plot. --> unfortunately can't cheat APLpy with the GILDAS header and I don't know how to edit it appropriately.. Use instead CASA to make the moment-0 map, see compare_mom0_CASA.py
 
 Last modified: May 16 2016
 
 History:
 --------
 16 May 2016:
+    - updated mom0 value to multiplied by chan width
     - fix bug, write out new FITS using the highest SNR array
     - look for ranges of channel that gives highest SNR
     - created script, working fine
+
+
+Note:
+-----
+    - useful to investigate the noise distribution per channel and if also see it in the moment-0 map, like in GILDAS
+    - useful to find the chan range with highest SNR
+    - but can't use to plot with APLpy, because header is not consistent with data
+    - altho. can use to plot with Matplotlib, but I can't figure out how to match coord. sys. with the HST image
 '''
 
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 from os.path import join
@@ -24,6 +34,12 @@ import pyfits
 import numpy as np
 from scipy.stats import norm
 
+font = {'family': 'Arial Narrow',
+        'weight': 'bold',
+        'size': 18.75}
+matplotlib.rc('font', **font)
+
+specRes = 21.528154         # km/s
 path = '/Users/admin/Research/RXJ1131/PdBI/data/15May16/'
 fits_cube = pyfits.open(join(path, 'centralizedCube4GILDAS.fits'))
 
@@ -35,8 +51,8 @@ minor_px = header['BMIN'] * 3600 / cdelt2
 BPA_deg = header['BPA']         # should be 13 deg
 
 # check histogram of each slice, offsource region
-start_channel = 124 - 1        # python indexing from 0
-nchan = 157-124+1
+start_channel = 127 - 1        # python indexing from 0
+nchan = 160 - start_channel + 1
 _plot = False
 
 for i in range(nchan):
@@ -63,7 +79,7 @@ for i in range(nchan):
 mom0_holder = np.zeros_like(fits_cube[0].data[0][0])
 for i in range(nchan):
     channel_number = start_channel + i
-    mom0_holder += fits_cube[0].data[0][channel_number]
+    mom0_holder += fits_cube[0].data[0][channel_number] * specRes
 (mu, sigma) = norm.fit(mom0_holder[15:220, 15:100].flatten())
 print mu, sigma
 
@@ -71,8 +87,6 @@ n, bins, patches = plt.hist(mom0_holder[15:220, 15:100].flatten(), bins=100, fac
 y = mlab.normpdf(bins, mu, sigma)
 l = plt.plot(bins, y, 'r--', linewidth=2)
 plt.show()
-# 0.000176980187679 0.0140376634548
-# --> with zero mean, but skewed to left. std is too high?
 
 
 # check by summing different number of channels
@@ -80,11 +94,11 @@ sigma_ch = np.std(fits_cube[0].data[0][10:120, :, :].flatten())      # in Jy
 # from GILDAS 124-156 == Python 123-156
 start_ = 123
 end_ = 156
-plt.hist(fits_cube[0].data[0][start_:end_, 15:220, 15:100].sum(axis=0).flatten(), bins=100)
-(mu, sigma) = norm.fit(fits_cube[0].data[0][start_:end_, 15:220, 15:100].sum(axis=0).flatten())
-print (" from array {}").format(fits_cube[0].data[0][start_:end_, 15:220, 15:100].sum(axis=0).flatten().std())
+plt.hist(fits_cube[0].data[0][start_:end_, 15:220, 15:100].sum(axis=0).flatten()*specRes, bins=100, normed=True)
+(mu, sigma) = norm.fit(fits_cube[0].data[0][start_:end_, 15:220, 15:100].sum(axis=0).flatten()*specRes)
+print (" from array {}").format(fits_cube[0].data[0][start_:end_, 15:220, 15:100].sum(axis=0).flatten().std()*specRes)
 print (" from bestfit {}").format(sigma)
-print (" expected: {}").format(sigma_ch * np.sqrt(end_-start_))
+print (" expected: {}").format(sigma_ch * np.sqrt(end_-start_)*specRes)
 # --> some channels have higher RMS
 
 cmap = plt.cm.jet
@@ -113,19 +127,21 @@ end_chan = 160
 mom0_SNR = np.zeros_like(fits_cube[0].data[0][0][110:145, 110:145])
 for i in range((end_chan-start_chan+1)):
     channel_number = start_chan + i
-    mom0_SNR += fits_cube[0].data[0][channel_number][110:145, 110:145]
-print ("max SNR: {:.2f}").format(mom0_SNR.max()/fits_cube[0].data[0][start_chan:end_chan, 15:220, 15:100].sum(axis=0).flatten().std())
-print (" map STD {} ").format(fits_cube[0].data[0][start_chan:end_chan, 15:220, 15:100].sum(axis=0).flatten().std())
+    mom0_SNR += fits_cube[0].data[0][channel_number][110:145, 110:145] * specRes
+print ("max SNR: {:.2f}").format(mom0_SNR.max()/(fits_cube[0].data[0][start_chan:end_chan, 15:220, 15:100].sum(axis=0).flatten().std()*specRes))
+print (" map STD {} ").format(fits_cube[0].data[0][start_chan:end_chan, 15:220, 15:100].sum(axis=0).flatten().std()*specRes)
 
 fig, ax = plt.subplots()
 fig.subplots_adjust(top=0.9)
 im = ax.imshow(mom0_SNR, origin="lower", norm=normC, cmap=cmap)
-cont_list = [fits_cube[0].data[0][start_chan:end_chan, 15:220, 15:100].sum(axis=0).flatten().std() * i for i in range(-6, 30, 3) if i != 0]
+cont_list = [fits_cube[0].data[0][start_chan:end_chan, 15:220, 15:100].sum(axis=0).flatten().std()*specRes * i for i in range(-6, 30, 3) if i != 0]
 ax.contour(mom0_SNR, cont_list, colors='black')
 plt.show()
 
 # the full image of the highest SNR range
-final = fits_cube[0].data[0][start_chan:end_chan, :, :].sum(axis=0)
+final = fits_cube[0].data[0][start_chan:end_chan, :, :].sum(axis=0) * specRes
+
+
 # ------------------------------------------------------------------
 # --> save this to a new fits file for APLpy plot
 # get header from GILDAS moment0 file
@@ -141,10 +157,6 @@ hdr.set('SPECSYS', header['SPECSYS'], 'patched from original cube')
 hdr.add_comment("This Moment-0 map is created with python, but I stole the header from GILDAS the moment0 map.")
 new_mom0 = 'centralizedCube4GILDAS-python_ch' + str(start_chan+1) + '-' + str(end_chan) + '_mom0.fits'
 pyfits.writeto(join(path, new_mom0), final, hdr, clobber=True)
-
-# CASA:
-# importfits(fitsimage=new_mom0,imagename=new_mom0.replace('.fits', '.image'))
-# imstat(imagename=new_mom0.replace('.fits', '.image'), region='offsource_region.crtf')
-# ---> consistent with what I got above
+fits_cube.close()
 
 
