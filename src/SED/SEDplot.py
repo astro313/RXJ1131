5315144991cd4179777d74faf27efab8fb014bc0
Daisy Leung <leungtszkuk@gmail.com>
2016-05-24 14:40:33 -0400

    MIPS 24 micron

--- src/SED/SEDplot.py
+++ src/SED/SEDplot.py
@@ -2,10 +2,15 @@
 
 Plot SED
 
-Last Modified: May 13 2016
+Last Modified: May 24 2016
 
 History
 -------
+May 24 2016:
+    - add using MC to get uncertainties on 24 um
+    - use MC best-fit for quote spectral index
+May 19 2016:
+    - add using MC to get uncertainties on IRAC MIR fit
 May 15 2016:
     - curve_fit to power_law working
     - but extrapolated flux > MIPS point
@@ -146,31 +151,102 @@ fluxErr_fg = np.asarray(tblWithErr_fg['Flux Err [mJy]'])
 
 # --------------- Fit slope -------------------------
 def powerf(x, prefact, index):
-    return prefact * x**index
-ppower = [1.0, 1.]
-import scipy.optimize as optimize
-p_fg, pcov_fg = optimize.curve_fit(powerf, wave_fg[3:7],
-                                   flux_fg[3:7], p0=[0.9, 1],
-                                   sigma=fluxErr_fg[3:7], absolute_sigma=True, maxfev=10000)
-perr_fg = np.sqrt(np.diag(pcov_fg))
+    '''
+    parameters
+    ----------
+    x: np.array
+        wavelength in micron
+    prefact: float
+    index: float
+    '''
+    # convert wavelength to freq, s.t. we can compare index with literature
+    from astropy.constants import c
+    x_hz = c.value / (x * 1e-6)
+    x_ghz = x_hz / 1.e9
+    return prefact * x_ghz**index
 
+def MC_simulation(ydata, yerr, func, p0, xdata, nsam=500, CI=1., debug=False):
+    '''
+    monte-carlo: generates random data points starting from the given data plus a random variation based on the systematic error, take into acct systematic uncertainties
+
+    Parameters
+    ----------
+    CI: float
+        1 <=> 68.3%
+
+    '''
+
+    ps = []
+    for i in range(nsam):
+        randomDelta = np.random.normal(0., yerr, size=len(ydata))
+        randomdataY = np.array(ydata) + randomDelta
+
+        if debug:
+            # verify it's drawing from distribution we expect
+            randomDelta = np.random.normal(0., yerr, size=1000)
+            count, bins, ignored = plt.hist(randomDelta, 30, normed=True)
+            plt.plot(bins, 1/(yerr * np.sqrt(2 * np.pi)) *
+                     np.exp(-(bins)**2 / (2 * yerr**2)), linewidth=2, color='r')
+            plt.show()
+        randomfit, randomcov = optimize.curve_fit(func,
+                                                  xdata, randomdataY, p0=p0)
+        ps.append(randomfit)
+
+    ps = np.array(ps)
+    mean_pfit = np.median(ps, 0)
+    Nsigma = CI
+    err_pfit_MC = Nsigma * np.std(ps, 0)
+    return ps, mean_pfit, err_pfit_MC
+
+
+def extrapolate_24_MC(ps):
+    '''
+    get a distribution of fluxes at 24 based on the MC fit parameters
+    '''
+
+    out_24 = []
+    for i in ps:
+        out_24.append(powerf(24., *i))
+    return np.array(out_24)
+
+
+ppower = [1.0, -1.]
+import scipy.optimize as optimize
+# get initial parameters for MC using curve_fit
+p_fg, pcov_fg = optimize.curve_fit(powerf, wave_fg[3:7],
+                                   flux_fg[3:7], p0=[0.1, -1.9],
+                                   sigma=fluxErr_fg[3:7], absolute_sigma=True)
+# perr_fg = np.sqrt(np.diag(pcov_fg))
 p_host, pcov_host = optimize.curve_fit(powerf, wave_host[3:7], flux_host[3:7],
                                        sigma=fluxErr_host[3:7], p0=ppower, absolute_sigma=True)
-perr_host = np.sqrt(np.diag(pcov_host))
+# perr_host = np.sqrt(np.diag(pcov_host))
+fg_MC, fg_MC_fit, fg_err_MC = MC_simulation(flux_fg[3:7], fluxErr_fg[3:7], powerf, p_fg, wave_fg[3:7])
+print "\n MC method :"
+print "pfit = ", fg_MC_fit
+print "perr = ", fg_err_MC
+host_MC, host_MC_fit, host_err_MC = MC_simulation(flux_host[3:7], fluxErr_host[3:7], powerf, p_host, wave_fg[3:7])
+print "\n MC method :"
+print "pfit = ", host_MC_fit
+print "perr = ", host_err_MC
+# ----> err similar from curve_fit, but best-fit differ because curve_fit is very sensitive to intial choice of parameter.. therefore use MC best-fit
+
+# spectral index from MC
+print("Spectral index alpha for foreground: {:.2f}+/-{:.2f}").format(fg_MC_fit[1], fg_err_MC[1])
+print("Spectral index alpha for RXJ1131: {:.2f}+/-{:.2f}").format(host_MC_fit[1], host_err_MC[1])
+
 
 # decompose 24 um point:
-flux_fg_24 = powerf(24, *p_fg)
-flux_host_24 = powerf(24, *p_host)
-# propagate uncertainty
-# sqrt((24^index * delta_amp)**2 + (24^(index-1) * delta_index * index * amp)**2)
-fluxerr_host_24 = np.sqrt((24.**p_host[1] * perr_host[0])**2 + (p_host[1] * p_host[0] * 24.**(p_host[1]-1) * perr_host[1])**2)
-fluxerr_fg_24 = np.sqrt((24.**p_fg[1] * perr_fg[0])**2 + (p_fg[1] * p_fg[0] * 24.**(p_fg[1]-1) * perr_fg[1])**2)
+flux_fg_24 = powerf(24, *fg_MC_fit)
+flux_host_24 = powerf(24, *host_MC_fit)
+# MC uncertainty
+fluxerr_fg_24 = np.std(extrapolate_24_MC(fg_MC))
+fluxerr_host_24 = np.std(extrapolate_24_MC(host_MC))
+
 
 print(" Total flux at MIPS {:.1f} um: {:.3f} mJy").format(wave_total[11], flux_total[11])
 print(" Host flux \t\t: {:.3f} +/- {:.3f} mJy").format(flux_host_24, fluxerr_host_24)
 print(" Fg flux \t\t: {:.3f} +/- {:.3f} mJy").format(flux_fg_24, fluxerr_fg_24)
 print(" Sum from extrapolated {:.3f} mJy").format((flux_fg_24 + flux_host_24))
-
 
 # extrapolate
 space = 0.2
@@ -185,8 +261,14 @@ plt.errorbar(wave_host[3:7], flux_host[3:7], fluxErr_host[3:7], fmt='.b', ms=8,
              capsize=8,
              elinewidth=4, capthick=1.5)
 
-plt.plot(xarray, powerf(xarray, *p_fg), 'r--', label='fit slope to FG IRAC')
-plt.plot(xarray, powerf(xarray, *p_host), 'b--', label='fit slope to host IRAC')
+# plot the fits in MC simulations for fg
+for f, h in zip(fg_MC, host_MC):
+    plt.plot(xarray, powerf(xarray, *f), color="#b44682", alpha=0.2)
+    plt.plot(xarray, powerf(xarray, *h), color="#67A9CF", alpha=0.2)
+
+# plot extrapolation
+plt.plot(xarray, powerf(xarray, *fg_MC_fit), 'r--', label='fit slope to FG IRAC')
+plt.plot(xarray, powerf(xarray, *host_MC_fit), 'b--', label='fit slope to host IRAC')
 
 # Plot the extrapolate flux at 24 um with error bars
 plt.errorbar(24., flux_host_24, fluxerr_host_24, fmt=".b", ms=8, ecolor='gray',
@@ -221,7 +303,7 @@ def savefigure(figure, f, verbose=True, dirStr='../../Figures/'):
             os.mkdir(dirStr)
         else:
             print "... Directory {:s} exists...".format(dirStr)
-    figure.savefig(dirStr + f + '.eps', dvi=600, box_inches='tight')
+    figure.savefig(dirStr + f + '.eps', dvi=600, bbox_inches='tight')
     print('... Saved Figure: {:s}').format(dirStr + f + '.eps')
 
 
@@ -253,8 +335,9 @@ xspan = wave_fg[3:7].max() - wave_fg[3:7].min()
 xspace = space * xspan
 xarray = np.linspace(wave_fg.min()-xspace, 24+xspace, 500)
 
-plt.plot(xarray, powerf(xarray, p_fg), 'r--', label='fit slope to FG IRAC')
-plt.plot(xarray, powerf(xarray, p_host), 'b--', label='fit slope to host IRAC')
+# plot extrapolation
+plt.plot(xarray, powerf(xarray, *fg_MC_fit), 'r--', label='fit slope to FG IRAC')
+plt.plot(xarray, powerf(xarray, *host_MC_fit), 'b--', label='fit slope to host IRAC')
 
 # Plot the extrapolate flux at 24 um with error bars
 plt.errorbar(24., flux_host_24, fluxerr_host_24, fmt=".b", ms=8, ecolor='gray',
